e20462bf68663cc471e1571fee4f4fb1bd760bfb
bhakti0801 <78205965+bhakti0801@users.noreply.github.com>
2025-09-08 00:02:59 +0530

    Delete pro1.c

--- pro1.c
+++
@@ -1,9 +0,0 @@
-// Write a program to input two numbers and display their sum.
-#include<stdio.h>
-int main(){
- int a,b, sum;
- scanf("%d %d", &a,&b);
- sum=a+b;
- printf("the sum of %d and %d is %d\n",a,b,sum);
- return 0;
-}
