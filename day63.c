1ef5b96672079fd11f834a396ec6a5c081622ecc
bhakti0801 <78205965+bhakti0801@users.noreply.github.com>
2025-12-02 16:15:50 +0530

    Create day63.c

---
+++ day63.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+
+    printf("Enter size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    if (k <= 0 || k > n) {
+        printf("Invalid value of k.\n");
+        return 0;
+    }
+
+    // Simple sorting (Bubble Sort) to sort array in ascending order
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    // kth smallest element is at index k-1
+    printf("The %dth smallest element: %d\n", k, arr[k - 1]);
+
+    return 0;
+}
