525a61fc0fc8bf230939cf94a83b8464cafb0fa4
bhakti0801 <78205965+bhakti0801@users.noreply.github.com>
2025-12-02 15:59:39 +0530

    Create day43_pro2.c

---
+++ day43_pro2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin); // read string with spaces
+
+    // Find the length of the string manually
+    int length = 0;
+    while (str[length] != '\0' && str[length] != '\n') {
+        length++;
+    }
+
+    // Check for palindrome
+    int isPalindrome = 1;
+    for (int i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - i - 1]) {
+            isPalindrome = 0;
+            break;
+        }
+    }
+
+    if (isPalindrome)
+        printf("The string is a palindrome.\n");
+    else
+        printf("The string is not a palindrome.\n");
+
+    return 0;
+}
