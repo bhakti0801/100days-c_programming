303d97f9c47aa9eae734765df7148888dd05b6ad
bhakti0801 <78205965+bhakti0801@users.noreply.github.com>
2025-12-02 16:33:05 +0530

    Create day92.c

---
+++ day92.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+// Define structure for Student
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+int main() {
+    struct Student students[5];
+
+    // Input details for 5 students
+    for (int i = 0; i < 5; i++) {
+        printf("Enter details for student %d:\n", i + 1);
+        
+        printf("Name: ");
+        scanf("%s", students[i].name);
+        
+        printf("Roll Number: ");
+        scanf("%d", &students[i].roll_no);
+        
+        printf("Marks: ");
+        scanf("%f", &students[i].marks);
+        
+        printf("\n");
+    }
+
+    // Print details of all students
+    printf("Student Details:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Student %d:\n", i + 1);
+        printf("Name: %s\n", students[i].name);
+        printf("Roll Number: %d\n", students[i].roll_no);
+        printf("Marks: %.2f\n\n", students[i].marks);
+    }
+
+    return 0;
+}
