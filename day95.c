b70e5564492eb2ecad7d50fcb79145d10f8ec466
bhakti0801 <78205965+bhakti0801@users.noreply.github.com>
2025-12-02 16:34:58 +0530

    Create day95.c

---
+++ day95.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+// Define structure for Student
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+// Function to find and return the top student
+struct Student getTopStudent(struct Student students[], int n) {
+    int maxIndex = 0;
+    for (int i = 1; i < n; i++) {
+        if (students[i].marks > students[maxIndex].marks) {
+            maxIndex = i;
+        }
+    }
+    return students[maxIndex];
+}
+
+int main() {
+    struct Student students[5];
+
+    // Input details for 5 students
+    for (int i = 0; i < 5; i++) {
+        printf("Enter details for student %d:\n", i + 1);
+        printf("Name: ");
+        scanf("%s", students[i].name);
+        printf("Roll Number: ");
+        scanf("%d", &students[i].roll_no);
+        printf("Marks: ");
+        scanf("%f", &students[i].marks);
+        printf("\n");
+    }
+
+    // Get top student
+    struct Student top = getTopStudent(students, 5);
+
+    // Print top student details
+    printf("Top Student Details:\n");
+    printf("Name: %s\n", top.name);
+    printf("Roll Number: %d\n", top.roll_no);
+    printf("Marks: %.2f\n", top.marks);
+
+    return 0;
+}
