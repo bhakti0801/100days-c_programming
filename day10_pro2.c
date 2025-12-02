cd640fa4bedc422196e5bc68070831d090d10e5a
bhakti0801 <78205965+bhakti0801@users.noreply.github.com>
2025-12-02 15:09:59 +0530

    Create day10_pro2.c

---
+++ day10_pro2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int day;
+
+    printf("Enter a number (1-7): ");
+    scanf("%d", &day);
+
+    switch(day) {
+        case 1:
+            printf("Day is Sunday\n");
+            break;
+        case 2:
+            printf("Day is Monday\n");
+            break;
+        case 3:
+            printf("Day is Tuesday\n");
+            break;
+        case 4:
+            printf("Day is Wednesday\n");
+            break;
+        case 5:
+            printf("Day is Thursday\n");
+            break;
+        case 6:
+            printf("Day is Friday\n");
+            break;
+        case 7:
+            printf("Day is Saturday\n");
+            break;
+        default:
+            printf("Invalid input! Enter a number between 1 and 7.\n");
+    }
+
+    return 0;
+}
