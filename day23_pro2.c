e4fc35ce60c799de7e7e25a70c8e45c061c78c1e
bhakti0801 <78205965+bhakti0801@users.noreply.github.com>
2025-12-02 15:31:14 +0530

    Create day23_pro2.c

---
+++ day23_pro2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int rows = 5;
+    int cols = 5;
+
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= cols; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
